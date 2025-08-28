2f505cf0db70dafdc5a954b22f0ef82af072859c
Michael Stucki <stcuthbertus@gmail.com>
2025-08-28 08:02:18 -0600

    Ex. 05

--- query_practice_05.sql
+++ query_practice_05.sql
@@ -1,10 +1,10 @@
--- total number of employees
+-- 01: total number of employees
 SELECT COUNT(emp_no) FROM employees;
 
--- total number of departments
+-- 02: total number of departments
 SELECT COUNT(dept_no) as number_departments FROM departments;
 
--- total number of employees within each department
+-- 03: total number of employees within each department
 SELECT d.dept_no, COUNT(e.emp_no) AS num_employees
 FROM employees e
 JOIN dept_emp de
@@ -12,3 +12,9 @@ ON e.emp_no = de.emp_no
 JOIN departments d
 ON de.dept_no = d.dept_no
 GROUP BY d.dept_no;
+
+-- 04: the five employees who worked at the company the longest
+SELECT e.emp_no, e.first_name, e.last_name, e.hire_date
+FROM employees e
+ORDER BY e.hire_date DESC
+LIMIT 5;
